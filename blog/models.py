9357b6ce6a6e863d298e4623d552160303cdb526
thetechnocrat <thetechnocrat.tech@gmail.com>
2021-01-24 16:45:29 +0530

    auth implemented

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    location=models.CharField(max_length=200)
+    
+
+#class Post(models.Model):
+    
+
+    
+#class Comment(models.Model):
+    
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,HttpResponse,HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
+def signup(request):
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            raw_password=form.cleaned_data.get('password1')
+            user=authenticate(username=username,password=raw_password)
+                
+            if user is not None:
+                login(request,user)
+                return redirect('home')
+            else:
+                return HttpResponse('registration failed')
+                
+            
+    else:
+        form=UserCreationForm()
+    return render(request,'signup.html',{'form':form})
+
+
+
+
+def logout(request):
+    if request.method=='POST':
+        logout(request)
+        return HttpResponseRedirect('login')
+
+
+def home(request):
+    #return HttpResponse('this is HOME of {{request.user.username}} <p>Your Username is : {{request.user}} </p>')
+    return render(request, 'index.html', {
+        'user': request.user,
+        })
